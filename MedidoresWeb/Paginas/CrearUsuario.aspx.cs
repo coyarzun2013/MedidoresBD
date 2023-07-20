73a69649a2811d61940f40a5bcc4f7d392709aae
punioyarzun <christianoyarzun94@gmail.com>
2023-07-20 16:18:25 -0400

    validador nuevo

--- MedidoresWeb/Paginas/CrearUsuario.aspx.cs
+++ MedidoresWeb/Paginas/CrearUsuario.aspx.cs
@@ -3,6 +3,7 @@ using MedidoresDB.DAL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -23,7 +24,7 @@ namespace MedidoresWeb.Paginas
             string rut = this.ruttxt.Text.Trim();
             if(rut.Contains("-"))
             {
-                if(validarRut(rut))
+                if(ValidaRut(rut))
                 {
                     usuario.Rut = this.ruttxt.Text.Trim();
                     usuario.Nombre = this.nombretxt.Text.Trim();
@@ -46,34 +47,49 @@ namespace MedidoresWeb.Paginas
             
         }
 
-        public bool validarRut(string rut)
+        public static bool ValidaRut(string rut)
         {
-            bool validar = false;
-            try
+            rut = rut.Replace(".", "").ToUpper();
+            Regex expresion = new Regex("^([0-9]+-[0-9K])$");
+            string dv = rut.Substring(rut.Length - 1, 1);
+            if (!expresion.IsMatch(rut))
             {
-                rut = rut.Trim();
-                rut = rut.ToUpper();
-                rut = rut.Replace(".", "");
-                rut = rut.Replace(",", "");
-                rut = rut.Replace("-", "");
-                int rutAux = int.Parse(rut.Substring(0,rut.Length - 1));
-                char dv = char.Parse(rut.Substring(rut.Length - 1,1));
-
-                int m = 0, s = 1;
-                for(;rutAux != 0; rutAux /= 10)
-                {
-                    s = (s + rutAux % 10 * (9 - m++ % 6)) % 11;
-                }
-                if(dv == (char)(s != 0 ? s + 47 : 75))
-                {
-                    validar = true;
-                }
+                return false;
             }
-            catch (Exception)
+            char[] charCorte = { '-' };
+            string[] rutTemp = rut.Split(charCorte);
+            if (dv != Digito(int.Parse(rutTemp[0])))
             {
-
+                return false;
             }
-            return validar;
+            return true;
+        }
+
+        public static string Digito(int rut)
+        {
+            int suma = 0;
+            int multiplicador = 1;
+            while (rut != 0)
+            {
+                multiplicador++;
+                if (multiplicador == 8)
+                    multiplicador = 2;
+                suma += (rut % 10) * multiplicador;
+                rut = rut / 10;
+            }
+            suma = 11 - (suma % 11);
+            if (suma == 11)
+            {
+                return "0";
+            }
+            else if (suma == 10)
+            {
+                return "K";
+            }
+            else
+            {
+                return suma.ToString();
+            }
         }
     }
 }
